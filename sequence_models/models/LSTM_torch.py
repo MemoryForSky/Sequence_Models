b608ebe5279a9d54a6f97d340df7a14de08b5881
Kevin <zhangxiaoqiang@ccx.cn>
2021-07-13 00:14:07 +0800

    update lstm

--- sequence_models/models/LSTM_torch.py
+++ sequence_models/models/LSTM_torch.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -24,3 +25,49 @@ class SeqsLSTM(nn.Module):
         output = self.dropout(output)
         logit = self.fc(output)
         return logit
+
+
+class BiLSTM(nn.Module):
+    #定义所有层
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, n_layers,
+                 bidirectional, dropout):
+        super().__init__()
+
+        # embedding 层
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+        # lstm 层
+        self.lstm = nn.LSTM(embedding_dim,
+                           hidden_dim,
+                           num_layers=n_layers,
+                           bidirectional=bidirectional,
+                           dropout=dropout,
+                           batch_first=True)
+
+        # 全连接层
+        self.fc = nn.Linear(hidden_dim * 2, output_dim)
+
+        # 激活函数
+        self.act = nn.Sigmoid()
+
+    def forward(self, text, text_lengths):
+        # text = [batch size,sent_length]
+        embedded = self.embedding(text)
+        # embedded = [batch size, sent_len, emb dim]
+
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths, batch_first=True)
+
+        packed_output, (hidden, cell) = self.lstm(packed_embedded)
+        # hidden = [batch size, num layers * num directions, hid dim]
+        # cell = [batch size, num layers * num directions, hid dim]
+
+        # 连接最后的正向和反向隐状态
+        hidden = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)
+
+        # hidden = [batch size, hid dim * num directions]
+        dense_outputs=self.fc(hidden)
+
+        # 激活
+        outputs=self.act(dense_outputs)
+
+        return outputs
